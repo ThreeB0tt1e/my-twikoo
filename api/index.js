f3954de8ae0f8464a39a30be84fac93bc7227fff
ThreeB0tt1e <492902597@qq.com>
2026-09-24 19:27:58 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,10 @@
+const Module = require('module');
+const originalLoad = Module._load;
+Module._load = function(request, parent, isMain) {
+  if (request === 'pushoo') {
+    return function() { return Promise.resolve(); };
+  }
+  return originalLoad.apply(this, arguments);
+};
+
 module.exports = require('twikoo-vercel');
